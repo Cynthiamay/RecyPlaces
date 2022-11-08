ef2801b469ca35cd700b8a90af28442f1f8e0af7
Mateus Amorim <amorim.mateussantos@outlook.com.br>
2022-11-07 22:09:36 -0300

    Initial Commit

---
+++ RecyPlaces/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RecyPlaces
+//
+//  Created by cynthia watanabe on 07/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
